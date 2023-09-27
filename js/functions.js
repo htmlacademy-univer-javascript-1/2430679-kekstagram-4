1702e1e5739c0109875a02afe808d6575ccfcba2
Апкаева Марина <apkaevam@mail.ru>
2023-09-27 13:40:41 +0500

    файл functions.js

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,6 @@ function isLessOrEqual (string, maxLength){
   return string.length <= maxLength;
 }
 
-console.log(isLessOrEqual('проверяемая строка', 20));
-console.log(isLessOrEqual('проверяемая строка', 18));
-console.log(isLessOrEqual('проверяемая строка', 10));
-
 function isPalindrom (passedString){
   const string = passedString.replaceAll(' ','').toLowerCase();
   let emptyString = '';
@@ -14,11 +10,6 @@ function isPalindrom (passedString){
   }
   return string === emptyString;
 }
-
-console.log(isPalindrom('топот'));
-console.log(isPalindrom('ДовОд'));
-console.log(isPalindrom('Кекс'));
-console.log(isPalindrom('Лёша на полке клопа нашёл '));
 
 function extractNumber (arg){
   const string = arg.toString();
@@ -30,12 +21,3 @@ function extractNumber (arg){
   }
   return parseInt(result, 10);
 }
-
-console.log(extractNumber('2023 год'));
-console.log(extractNumber('ECMAScript 2022'));
-console.log(extractNumber('1 кефир, 0.5 батона'));
-console.log(extractNumber('агент 007'));
-console.log(extractNumber('а я томат'));
-console.log(extractNumber(2023));
-console.log(extractNumber(-1));
-console.log(extractNumber(1.5));
