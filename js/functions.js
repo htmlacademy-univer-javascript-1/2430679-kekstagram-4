53d04f781d4f8a25e25357c9c3c0e8d76d1d7c72
Апкаева Марина <apkaevam@mail.ru>
2023-09-27 13:29:24 +0500

    Функция для извлечения числа из строки

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,23 @@ console.log(isPalindrom('топот'));
 console.log(isPalindrom('ДовОд'));
 console.log(isPalindrom('Кекс'));
 console.log(isPalindrom('Лёша на полке клопа нашёл '));
+
+function extractNumber (arg){
+  const string = arg.toString();
+  let result = '';
+  for(let i = 0; i < string.length; i++){
+    if(!Number.isNaN(parseInt(string[i], 10))){
+      result += string[i];
+    }
+  }
+  return parseInt(result, 10);
+}
+
+console.log(extractNumber('2023 год'));
+console.log(extractNumber('ECMAScript 2022'));
+console.log(extractNumber('1 кефир, 0.5 батона'));
+console.log(extractNumber('агент 007'));
+console.log(extractNumber('а я томат'));
+console.log(extractNumber(2023));
+console.log(extractNumber(-1));
+console.log(extractNumber(1.5));
