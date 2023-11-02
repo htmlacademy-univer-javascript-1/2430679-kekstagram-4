b9de1037c5d5c6b56d20d8f8e11ce6023837c180
Апкаева Марина <apkaevam@mail.ru>
2023-11-02 16:31:37 +0500

    Функция создания миниатюры

--- js/pictures.js
+++ js/pictures.js
@@ -1,1 +1,11 @@
+const pictureFragments = document.createDocumentFragment();
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
+const createPicture = ({url, description, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  return pictureElement;
+};
