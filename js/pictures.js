6c6b10f456f12d5333b06c166969ceb3994c8ad2
Апкаева Марина <apkaevam@mail.ru>
2023-11-23 20:21:43 +0500

    Загрузка комментариев по частям

--- js/pictures.js
+++ js/pictures.js
@@ -1,22 +1,21 @@
-import { openPicture } from './fullsizePicture.js';
+import {openPicture} from './fullsizePicture.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const createPicture = (picture) => {
-  const {url, description, likes, comments} = picture;
+const createPicture = ({url, description, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__img').alt = description;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.addEventListener('click', () => {
-    openPicture(picture);
+    openPicture({url, description, likes, comments});
   });
   return pictureElement;
 };
 
-const createPictures = (pictures) => {
+const getPictures = (pictures) => {
   const pictureFragments = document.createDocumentFragment();
   pictures.forEach((picture) => {
     pictureFragments.append(createPicture(picture));
@@ -24,4 +23,4 @@ const createPictures = (pictures) => {
   picturesContainer.append(pictureFragments);
 };
 
-export {createPictures};
+export {getPictures};
